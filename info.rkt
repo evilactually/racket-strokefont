eb5c76bd60f7ea3163a25aa16b95db8d79da48b0
Vlad Shcherbakov <vlad.shcherbakov@gmail.com>
2025-03-09 17:50:58 -0600

    Initial commit

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "strokefont")
 (define deps '("base" "control"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "pict-lib" "draw-lib" "testing-util-lib" "rackunit-lib"))
 (define scribblings '(("scribblings/strokefont.scrbl" ())))
 (define pkg-desc "Simple to use Hershey stroke font")
 (define version "0.1")
